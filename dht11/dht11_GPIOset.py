f37aacb7b595380ae4788f6720cfdfdd1babe9dc
abartaa <denis.bartley@gmail.com>
2020-01-14 21:11:37 +0000

    Added Temp to CSV Function

--- dht11/dht11_GPIOset.py
+++ dht11/dht11_GPIOset.py
@@ -23,22 +23,25 @@ try:
 	    result = instance.read()
 	    if result.is_valid():
 	        print("Last valid input: " + str(datetime.datetime.now()))
-		#temp = result.temperature()
-		print("Temperature: %-3.1f C" % result.temperature)
-                print("Humidity: %-3.1f %%" % result.humidity)
-		if result.temperature > 18.4:
-			GPIO.setup(24, GPIO.OUT)
-			GPIO.output(24, True)
-			GPIO.setup(26, GPIO.OUT)
-                        GPIO.output(26, False)
-			print("Green Light")
-		else:
-			GPIO.setup(26, GPIO.OUT)
-                        GPIO.output(26, True)
-			GPIO.setup(24, GPIO.OUT)
-                        GPIO.output(24, False)
-                        print("Red Lights")
-	    time.sleep(1)
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
+            with open("../sens_temp.csv", "a") as log:
+                #while True:
+                if result.temperature != 0.0:
+                    log.write("{0},{1}\n".format(time.strftime("%Y-%m-%d %H:%M:%S"),str(result.temperature)))
+            if result.temperature > 18.4:
+                GPIO.setup(24, GPIO.OUT)
+                GPIO.output(24, True)
+                GPIO.setup(26, GPIO.OUT)
+                GPIO.output(26, False)
+                print("Green Light")
+            else:
+                GPIO.setup(26, GPIO.OUT)
+                GPIO.output(26, True)
+                GPIO.setup(24, GPIO.OUT)
+                GPIO.output(24, False)
+                print("Red Lights")
+	    time.sleep(5)
 
 except KeyboardInterrupt:
     print("Cleanup")
